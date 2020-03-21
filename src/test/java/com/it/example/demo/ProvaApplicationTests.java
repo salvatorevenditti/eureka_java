6fe7b219ed8f683e4ab27dac95bd203833f215d3
Salvatore Venditti <salvatore.venditti@accenture.com>
2020-03-21 18:06:40 +0100

    first commit

---
+++ src/test/java/com/it/example/demo/ProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
